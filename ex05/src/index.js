54913a2af744a803dcb39be63de5a4a0c4d12551
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 00:18:51 +0100

    Create index.js

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+// Write code below this line
+var dress = "little dress";  
+function myDress(){
+  var dress = "ball gown";
+// Write code above this line
+ return dress;
+}
+// Write code below this line
+  
+// Write code above this line
+console.log(dress)
+
+module.exports = functionPrintInfo;
