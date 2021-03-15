2718a22ac505702848be6c19d5548ceba17c0806
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 00:35:58 +0100

    Create index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,16 @@
+// Write code below this line
+   var sum = 0;
+   function addThree(num){
+     sum = sum + num;
+   }
+   function addFive(num){
+     sum = sum + num;
+   }
+addThree(3);
+addFive(5);
+// Write code above this line
+
+module.exports = {
+  addThree,
+  addFive
+};
