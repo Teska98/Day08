a6f45ae3ddd1e7f2f0410f5f05068046696b065b
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 00:59:49 +0100

    Create index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+  function isLess (a, b){
+  // Only change code above this line
+  if (a < b) {   
+     // Only change code below this line
+    return true;
+     // Only change code above this line
+  } else {
+     // Only change code below this line
+    return false;
+     // Only change code above this line
+  }
+  // Only change code below this line
+}
+isLess(10, 15);
+isLess(15, 10);    
+console.log(isLess(a, b));
+console.log(isLess(a, b));
+// Only change code above this line   
+module.exports = funcWithArg;
