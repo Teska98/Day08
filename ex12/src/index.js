c8bfba5b38cdca4bc1afb67b7a65326deb66bc88
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 09:24:42 +0100

    Update index.js

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -2,5 +2,6 @@
    function randomInteger(){
       return Math.floor(Math.random() * 30);
    }
+console.log(randomInteger);
 // Only change code above this line   
 module.exports = randomInteger;
