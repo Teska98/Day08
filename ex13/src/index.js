74737df4640be77bf2f139bf619770fb2b56b608
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 09:17:35 +0100

    Create index.js

---
+++ ex13/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+   function randomRangeNumber(minNumber, maxNumber){
+      return Math.floor(Math.random() * (maxNumber - minNumber +1)) + minNumber;
+   }
+console.log(randomRangeNumber(2, 30))
+// Only change code above this line   
+module.exports = randomInteger;
