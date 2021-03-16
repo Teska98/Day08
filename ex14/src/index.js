443f90decf3aff0498c964552497d5bf36628c41
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-16 09:22:44 +0100

    Create index.js

---
+++ ex14/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+   function convertStrToInt(str){
+     var str = parseInt ();
+     return str;
+   }
+console.log(convertStrToInt("456"))
+// Only change code above this line   
+module.exports = convertStrToInt;
